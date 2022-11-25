548d9a473b9c399528fe1bfb721505eff088e836
sangy00n <dbs1367@postech.ac.kr>
2022-11-25 16:35:46 +0900

    Implement: run function in package.scala

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -91,8 +91,12 @@ package object nodescala {
 
     /** Creates a cancellable context for an execution and runs it.
      */
-    def run()(f: CancellationToken => Future[Unit]): Subscription = ???
-
+    def run()(f: CancellationToken => Future[Unit]): Subscription = {
+      val ctSource = CancellationTokenSource()
+      val ct = ctSource.cancellationToken
+      f(ct)
+      ctSource
+    }
   }
 
   /** Adds extension methods to future objects.
